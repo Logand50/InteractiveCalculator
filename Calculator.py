f8a68cadbc80a1d6cc5ea0c692972f9715b94f70
Logand50 <128848531+Logand50@users.noreply.github.com>
2023-06-24 18:17:00 -0400

    Update Calculator.py
    Added new features to the calculator

--- Calculator.py
+++ Calculator.py
@@ -1,41 +1,46 @@
 from mpg_calc import calc
 from smpl_intr import simple_interest_calculator
+from anagram_generator import search_for_anagrams
 import sys
 
-    
-print()
-print('\tINTERACTIVE CALCULATOR')
-print()
-print('by: Logan Dye')
-print()
-print('Hello! \n\tWhich calculator program would you like to use? ')
-print('Hint: Type options to view the calculator choices')
-while True:
-    options = ['MPG Calculator', 'Interest Calculator']
-    user_selection = input()
-    if user_selection == 'options':
-        print('\t', end='')
-        print(*options, sep=', ')
-    elif user_selection == 'Interest':
-        while True:
-            print('Simple or Compound?')
-            interest_choice = input()
-            if interest_choice == 'Simple':
-                simple_interest_calculator()
-                break
-            elif interest_choice == 'Compound':
-                print('That feature is not available yet.')
-            elif interest_choice == 'back':
-                break
-            else:
-                print()
-                print('Not a valid response. \n Enter back to go to the main menu.')
-    elif user_selection == 'MPG Calculator':
-        calc()
-    elif user_selection == 'exit':
-        sys.exit()
-    else:
-        print('Please enter a valid command.')
+while True:   
+    print()
+    print('\tINTERACTIVE CALCULATOR')
+    print()
+    print('by: Logan Dye')
+    print()
+    print('Hello! \n\tWhich calculator program would you like to use? ')
+    print('Hint: Type options to view the calculator choices')
+    while True:
+        options = ['MPG Calculator', 'Interest Calculator', 'Anagram Generator']
+        user_selection = input()
+        if user_selection == 'options':
+            print('\t', end='')
+            print(*options, sep=', ')
+        elif user_selection == 'Interest':
+            while True:
+                print('Simple or Compound?')
+                interest_choice = input()
+                if interest_choice == 'Simple':
+                    simple_interest_calculator()
+                    break
+                elif interest_choice == 'Compound':
+                    print('That feature is not available yet.')
+                elif interest_choice == 'back':
+                    break
+                else:
+                    print()
+                    print('Not a valid response. \n Enter back to go to the main menu.')
+        elif user_selection == 'MPG Calculator':
+            calc()
+            break
+        elif user_selection == 'Anagram':
+            search_for_anagrams()
+            break
+        elif user_selection == 'exit':
+            sys.exit()
+        else:
+            print('Please enter a valid command.')
 
 
 
